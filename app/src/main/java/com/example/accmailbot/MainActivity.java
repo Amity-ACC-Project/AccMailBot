741b02d579a95b52c73b6f9fca6dd974d663a532
HarshSinghRajawat <harshsinghrajawat86@gmail.com>
2021-02-23 15:31:58 +0530

    Removed Unnecessary Dependencies and Classes For now

--- app/src/main/java/com/example/accmailbot/MainActivity.java
+++ app/src/main/java/com/example/accmailbot/MainActivity.java
@@ -1,18 +1,15 @@
 package com.example.accmailbot;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.Observer;
 import androidx.work.Constraints;
 import androidx.work.Data;
 import androidx.work.NetworkType;
-import androidx.work.WorkInfo;
 
 import androidx.work.OneTimeWorkRequest;
 import androidx.work.WorkManager;
 import androidx.work.WorkRequest;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -29,36 +26,23 @@ public class MainActivity extends AppCompatActivity {
          TextView textView=(TextView) findViewById(R.id.txt);
          Button button=(Button)findViewById(R.id.button);
 
-        encryptData("harshsinghrajawat86@gmail.com","Test","<h1>Hi</h1> From Java");
+        encryptData("harshsinghrajawat86@gmail.com,sahil777vishwakarma@gmail.com","Test","<h1>This Mail is from AccMailBot</h1><br><b>Hi</b> From Java");
 
 
         textView.setText(UserDetails.refresh_token);
-        AsyncGetToken Task=new AsyncGetToken();
-        AsyncPostData Task1=new AsyncPostData();
-        //Task.execute();Task1.execute();
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 Data data=new Data.Builder()
-                        .putString("RawData",UserDetails.raw_data)
+                        .putString("token",UserDetails.access_token)
                         .build();
                 Constraints constraints=new Constraints.Builder()
                         .setRequiredNetworkType(NetworkType.CONNECTED)
                         .build();
-                WorkRequest Request=new OneTimeWorkRequest.Builder(HttpGetWorker.class)
+                WorkRequest PostRequest=new OneTimeWorkRequest.Builder(HttpRequestWorker.class)
                         .setInputData(data)
                         .setConstraints(constraints)
-                        .addTag("getToken")
-                        .build();
-                Data data1=new Data.Builder()
-                        .putString("token",UserDetails.access_token)
-                        .build();
-                Constraints constraints1=new Constraints.Builder()
-                        .setRequiredNetworkType(NetworkType.CONNECTED)
-                        .build();
-                WorkRequest PostRequest=new OneTimeWorkRequest.Builder(HttpPostWorker.class)
-                        .setInputData(data1)
-                        .setConstraints(constraints1)
                         .addTag("PostData")
                         .build();
 
--- app/src/main/java/com/example/accmailbot/UserDetails.java
+++ app/src/main/java/com/example/accmailbot/UserDetails.java
@@ -3,7 +3,6 @@ package com.example.accmailbot;
 public class UserDetails {
     //Urls
     public static final String TokenUrl="https://developers.google.com/oauthplayground/refreshAccessToken";
-    //public static final String PostUrl="https://developers.google.com/oauthplayground/sendRequest";
     public static final String PostUrl="https://content-gmail.googleapis.com/gmail/v1/users/webproject4551%40gmail.com/messages/send?";
     //Required Data
     public static final String token_uri="https://oauth2.googleapis.com/token";
@@ -12,6 +11,7 @@ public class UserDetails {
     public static final String refresh_token="1//04KtKnUcd16M4CgYIARAAGAQSNwF-L9IrUoIF6HU7BthQIW8Ny6zqp0y0gWhF5KxC8IIpIxSsfay7lxH4CDdNOAFsCee6rBuudcw";
     public static String access_token=null;
     public static String raw_data="";
+    public static int intervals=1;
 
 
 }
